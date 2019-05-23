0023a37410d9c7887665bab85ffe366361c7dd4f
brunamello <brunacrespomello@gmail.com>
2019-05-23 13:37:52 +0100

    Update 23-05
    Signed-off-by: brunamello <brunacrespomello@gmail.com>

--- contas/views.py
+++ contas/views.py
@@ -20,8 +20,26 @@ def Listagem(request):
 def nova_transacao(request):
     data = {}
     form = TransacaoForm(request.POST or None)
+
     if form.is_valid():
         form.save()
         return redirect('url_listagem')
     data['form'] = form
     return render(request, 'contas/form.html', data)
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
